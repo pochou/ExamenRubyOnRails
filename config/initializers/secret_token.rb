e7c81d2ae3f49e97fdc46c07c7c13917f8015ed9
Pochou <pochou@pochou-N70SV.(none)>
2013-03-07 14:38:32 +0100

    first commit depot propre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '55bdfbbf6d16cb80baf880805ecf03d68514998ce6fba2682c2d4c1f4a3ebc1e1ebd16a2c92e4eeeb61503ff0449c9667c43d68d2d4044c9c431b9cdba155dd8'
